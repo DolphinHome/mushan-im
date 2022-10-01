862ebd6ab76024a5ab095f5da35f7c3387040337
wangbin <2108097401>
2022-10-01 22:13:07 +0800

    into

---
+++ mushan-im-ui/pages/meet/meet.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		接电话
+		<button @click="meetTo">接听</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				source:'',
+			}
+		},
+		onLoad(e){
+			this.source = e.source
+		},
+		methods: {
+			meetTo(){
+				//推送消息接听
+				let user = this.$store.state.user;
+				let msg = {to:this.source,source:user,msg:null,type:3};
+				this.$store.dispatch('websocketSend',JSON.stringify(msg))
+				//跳转页面进行通话
+				
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
